6ae618abb6f88586e0683ea24060fc16568d2309
Areyzo <Thapaprabin071@gmail.com>
2025-05-18 07:35:09 +0545

    tree chopping fix
    need fix diagonal walking (diagonal movement is faster than horizontal or vertical movement)

--- scenes/character/player/player.gd
+++ scenes/character/player/player.gd
@@ -4,15 +4,15 @@ extends CharacterBody2D
 @onready var damage_box = $flip/damagebox
 @onready var flip = $flip
 
-
-const SPEED = 300.0
+const SPEED = 150
 
 var doChop = false
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("attack"):
+		print_debug("Do chop called")
 		_do_chop()
-		print_debug("Do chop")
+		
 
 	var direction = Vector2.ZERO
 	direction.x = Input.get_axis("left", "right")
@@ -32,7 +32,6 @@ func _set_animation():
 		flip.scale.x = -1
 	elif velocity.x > 0: 
 		flip.scale.x = 1
-		
 	if doChop: return
 	
 	if velocity:
@@ -41,34 +40,30 @@ func _set_animation():
 		animated_sprite_2d.play("idle")
 
 func _do_chop():
-	if doChop: return
-	
+	if doChop:
+		return
+		
 	doChop = true
 	animated_sprite_2d.play("chop")
 
 func _hit_tree():
 	var hasCollision = len(damage_box.get_overlapping_bodies()) > 0
 	if not hasCollision:
-		print_debug("nocollision")
 		return
 	var collisionBody = damage_box.get_overlapping_bodies()[0]
 	
 	if collisionBody in get_tree().get_nodes_in_group("trees"):
 		collisionBody.get_hit(flip.scale.x)
-		print_debug("collision")
 
 func _on_animated_sprite_2d_animation_finished() -> void:
 	doChop = false
-	
+	print_debug("animation ended")
 	animated_sprite_2d.play("idle")
 
-	
-func _on_animated_sprite_2d_sprite_frames_changed() -> void:
-	if animated_sprite_2d == null: return
+func _on_animated_sprite_2d_frame_changed() -> void:
+	if animated_sprite_2d == null:
+		return
 	
 	var frame = animated_sprite_2d.frame
-	
 	if animated_sprite_2d.animation == "chop" && frame == 3:
 		_hit_tree()
-		print_debug("hit tree")
-	

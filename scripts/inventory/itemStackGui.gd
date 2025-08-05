60304e6f1eaf51299b636cb99c84c5187038211c
AayushmanBajracharya <aayushmanbajracharya5@gmail.com>
2025-08-05 23:12:03 +0545

    log can be seen on the inventory after picking up

--- scripts/inventory/itemStackGui.gd
+++ scripts/inventory/itemStackGui.gd
@@ -14,6 +14,18 @@ func update():
 
 	itemSprite.visible = true
 	itemSprite.texture = inventorySlot.item.texture
+	
+	# Set item to display at 32x32 pixels in inventory
+	if itemSprite.texture:
+		var texture_size = itemSprite.texture.get_size()
+		var desired_size = Vector2(32, 32)
+		var scale_factor = Vector2(
+			desired_size.x / texture_size.x,
+			desired_size.y / texture_size.y
+		)
+		# Use the smaller scale factor to maintain aspect ratio
+		var uniform_scale = min(scale_factor.x, scale_factor.y)
+		itemSprite.scale = Vector2(uniform_scale, uniform_scale)
 
 	if inventorySlot.amount > 1:
 		amountLabel.visible = true
